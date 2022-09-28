d5bce9f5bedd2f2b8a3e702fa395b1aa71b59fe0
samahali <samahali9226@gmail.com>
2022-09-28 08:51:26 +0200

    change folders name of my app

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
 source  env/bin/activate
+
+cd /var/lib/jenkins/workspace/django_cicd/app
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "Migrations Done :D :D"
+
+cd /var/lib/jenkins/workspace/django_cicd
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo  "$PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo  "Gunicorn has been started"
+
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
