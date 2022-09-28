e9bc9aaf404f0676c57b70f65bdce2e004b2fa58
samahali <samahali9226@gmail.com>
2022-09-28 10:20:50 +0200

    change the path of my app in jenkins workspace from django_cicd to django_cicd@2

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,14 +2,14 @@
 
 source  env/bin/activate
 
-cd /var/lib/jenkins/workspace/django_cicd/app
+cd /var/lib/jenkins/workspace/django_cicd@2/app
 
 python3 manage.py makemigrations
 python3 manage.py migrate
 
 echo "Migrations Done :D :D"
 
-cd /var/lib/jenkins/workspace/django_cicd
+cd /var/lib/jenkins/workspace/django_cicd@2
 
 sudo cp -rf gunicorn.socket /etc/systemd/system/
 sudo cp -rf gunicorn.service /etc/systemd/system/
